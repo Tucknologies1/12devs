e0e85c721ec7b62740b5edf89e07687d07aeb128
Stephen Fulljames <stephen@fulljames.net>
2011-12-14 19:10:44 +0000

    Added scrollableArea viewmodel and tests/

--- site.js
+++ site.js
@@ -8,9 +8,93 @@ site.models.Gallery = function() {
     var self = this;
 
     this.itemsObservables = ko.observableArray();
+
+    // create instance of Scrollable viewmodel
+    this.scrollable = new site.models.ScrollableArea();
 }
 
+// define Scrollable viewmodel
+site.models.ScrollableArea = function() {
+    // create self reference for use inside functions
+    var self = this;
 
+    this.scrollThreshold = ko.observable(0); // total width of content area
+    this.scrollOffset = ko.observable(0); // left/right padding to avoid going under buttons
+    this.scrollOffsetPadding = ko.observable(0); // extra padding for visual separation of buttons and selected content item
+    this.contentSize = ko.observable(0); // measured width of all content items
+    this.scrollValue = ko.observable(0); // where the content should be scrolled to
+    this.scrollInitTo = ko.observable('left'); // should the content scroll to left or right when it becomes scrollable
+    this.scrollClickStep = ko.observable(200);
+
+    this.selectedOffLeft = ko.observable(false); // is selected item hidden to the left of the visible area
+    this.selectedOffRight = ko.observable(false); // is selected item hidden to the right of the visible area
+
+    this.measureContent = null;
+    this.positionContent = null;
+    this.recalculateScrollValue = null;
+
+    this.isScrollable = ko.dependentObservable({
+        read: function () {
+            if (self.contentSize() > self.scrollThreshold()) {
+                self.scrollValue((self.scrollInitTo() == 'right') ? self.scrollThreshold() - self.contentSize() : 0);
+                return true;
+            } else {
+                self.scrollValue(0);
+                return false;
+            }
+        }
+    });
+    this.calculatedScrollValue = ko.dependentObservable({
+        read: function () {
+            var newCalculatedScrollValue = self.scrollValue() - (self.scrollOffset() + self.scrollOffsetPadding());
+            return (self.isScrollable()) ? newCalculatedScrollValue : 0;
+        },
+        write: function (value) {
+            var newScrollValue = value + self.scrollOffset() + self.scrollOffsetPadding();
+            self.scrollValue(newScrollValue);
+        }
+    });
+    this.calculatedScrollValue.subscribe(function (value) {
+        if (self.positionContent) self.positionContent(value);
+    });
+    this.canScrollLeft = ko.dependentObservable({
+        read: function () {
+            return (self.isScrollable()) ? (self.calculatedScrollValue() != self.scrollOffset()) : false;
+        }
+    });
+    this.canScrollRight = ko.dependentObservable({
+        read: function () {
+            var offset = self.scrollThreshold() - self.contentSize();
+            return (self.isScrollable()) ? (self.scrollValue() != offset) : false;
+        }
+    });
+
+    this.scrollContent = function (e) {
+
+        var direction = e.target.getAttribute('data-direction');
+        var newCalculatedScrollValue;
+
+        if (direction == 'left') {
+            newCalculatedScrollValue = self.calculatedScrollValue() + self.scrollClickStep();
+            if (newCalculatedScrollValue > self.scrollOffset()) {
+                newCalculatedScrollValue = self.scrollOffset();
+            }
+        } else if (direction == 'right') {
+            newCalculatedScrollValue = self.calculatedScrollValue() - self.scrollClickStep();
+            var limit = self.scrollThreshold() - (self.contentSize() + self.scrollOffset() + self.scrollOffsetPadding());
+
+            if (newCalculatedScrollValue < limit) {
+                newCalculatedScrollValue = limit;
+            }
+        } else {
+            newCalculatedScrollValue = self.calculatedScrollValue();
+        }
+
+        self.calculatedScrollValue(newCalculatedScrollValue);
+
+        e.preventDefault();
+    };
+}
 
 // using jQuery for convenience but any other JS library or plain onload function would also be fine
 $(function() {
--- tests/tests.js
+++ tests/tests.js
@@ -5,6 +5,108 @@ module("Main Model", {
 
 test("When creating a new empty Gallery viewmodel", function() {
     var Gallery = new site.models.Gallery();
+    var scrollable = Gallery.scrollable;
 
     equal(Gallery.itemsObservables().length, 0, "there should be no values in itemsObservables");
+
+});
+
+module("Scrollable Area View Model", {
+    setup: function () {
+    }
+});
+
+test("When initialising scrollableArea UI scrolling values", function() {
+    var scrollableArea = new site.models.ScrollableArea(true);
+    scrollableArea.measureContent = function () { return 0; };
+
+    equal(scrollableArea.isScrollable(), false, " scrollableArea isScrollable should initially be false");
+    equal(scrollableArea.canScrollLeft(), false, " scrollableArea canScrollLeft should initially be false");
+    equal(scrollableArea.canScrollRight(), false, " scrollableArea canScrollRight should initially be false");
+    equal(scrollableArea.scrollInitTo(), "left", " scrollableArea scrollInit should initially be 'left'");
+    equal(scrollableArea.scrollThreshold(), 0, " scrollableArea scrollThreshold should initially be 0");
+    equal(scrollableArea.scrollOffset(), 0, " scrollableArea scrollOffset should initially be 0");
+    equal(scrollableArea.scrollValue(), 0, " scrollableArea scrollValue should initially be 0");
+    equal(scrollableArea.calculatedScrollValue(), 0, " scrollableArea calculatedScrollValue should initially be 0");
+    equal(scrollableArea.contentSize(), 0, " scrollableArea contentSize should initially be 0");
+});
+
+test("When changing the size of the content when scrollInitTo is 'left' (default)", function () {
+    var scrollableArea = new site.models.ScrollableArea(true);
+    scrollableArea.measureContent = function () { return 0; };
+
+    scrollableArea.scrollThreshold(980);
+    scrollableArea.contentSize(400);
+
+    equal(scrollableArea.scrollThreshold(), 980, " scrollableArea scrollThreshold should be initially be 980");
+    equal(scrollableArea.contentSize(), 400, " scrollableArea contentSize should initially be 400");
+    equal(scrollableArea.isScrollable(), false, " scrollableArea isScrollable should be set to false");
+    equal(scrollableArea.scrollValue(), 0, " scrollableArea scrollValue should be set to 0");
+    equal(scrollableArea.calculatedScrollValue(), 0, " scrollableArea calculatedScrollValue should be set to 0");
+
+    scrollableArea.contentSize(1000);
+
+    equal(scrollableArea.contentSize(), 1000, " scrollableArea contentSize should be updated to 1000");
+    equal(scrollableArea.isScrollable(), true, " scrollableArea isScrollable should be set to true");
+    equal(scrollableArea.canScrollLeft(), false, " scrollableArea canScrollLeft should be set to false");
+    equal(scrollableArea.canScrollRight(), true, " scrollableArea canScrollRight should be set to true");
+    equal(scrollableArea.scrollOffset(), 0, " scrollableArea scrollOffset should be set to 0");
+    equal(scrollableArea.scrollValue(), 0, " scrollableArea scrollValue should be set to 0");
+    equal(scrollableArea.calculatedScrollValue(), 0, " scrollableArea calculatedScrollValue should be set to 0");
+
+    scrollableArea.contentSize(900);
+
+    equal(scrollableArea.contentSize(), 900, " scrollableArea contentSize should be updated to 900");
+    equal(scrollableArea.isScrollable(), false, " scrollableArea isScrollable should be set to false");
+    equal(scrollableArea.canScrollLeft(), false, " scrollableArea canScrollLeft should be set to false");
+    equal(scrollableArea.canScrollRight(), false, " scrollableArea canScrollRight should be set to false");
+    equal(scrollableArea.scrollValue(), 0, " scrollableArea scrollValue should be set to 0");
+    equal(scrollableArea.calculatedScrollValue(), 0, " scrollableArea calculatedScrollValue should be set to 0");
+});
+
+test("When changing the size of the content when scrollInitTo is 'right'", function () {
+    var scrollableArea = new site.models.ScrollableArea(true);
+    scrollableArea.measureContent = function () { return 0; };
+
+    scrollableArea.scrollInitTo('right');
+    scrollableArea.scrollThreshold(980);
+    scrollableArea.contentSize(1000);
+
+    equal(scrollableArea.contentSize(), 1000, " scrollableArea contentSize should be updated to 1000");
+    equal(scrollableArea.isScrollable(), true, " scrollableArea isScrollable should be set to true");
+    equal(scrollableArea.canScrollLeft(), true, " scrollableArea canScrollLeft should be set to true");
+    equal(scrollableArea.canScrollRight(), false, " scrollableArea canScrollRight should be set to false");
+    equal(scrollableArea.scrollOffset(), 0, " scrollableArea scrollOffset should be set to 0");
+    equal(scrollableArea.scrollValue(), -20, " scrollableArea scrollValue should be set to -20");
+    equal(scrollableArea.calculatedScrollValue(), -20, " scrollableArea calculatedScrollValue should be set to -20");
+});
+
+test("When changing the size of the scroll offset", function () {
+    var scrollableArea = new site.models.ScrollableArea(true);
+    scrollableArea.measureContent = function () { return 0; };
+
+    scrollableArea.scrollThreshold(980);
+    scrollableArea.contentSize(1000);
+    scrollableArea.scrollOffset(20);
+    scrollableArea.scrollInitTo('right');
+
+    equal(scrollableArea.scrollOffset(), 20, " scrollableArea scrollOffset should be 20");
+    equal(scrollableArea.canScrollLeft(), true, " scrollableArea canScrollLeft should be set to true");
+    equal(scrollableArea.canScrollRight(), false, " scrollableArea canScrollRight should be set to false");
+    equal(scrollableArea.scrollValue(), -20, " scrollableArea scrollValue should be set to -40");
+    equal(scrollableArea.calculatedScrollValue(), -40, " scrollableArea calculatedScrollValue should be set to -40");
+});
+
+test("When writing to the calculated scroll value", function () {
+    var scrollableArea = new site.models.ScrollableArea(true);
+    scrollableArea.measureContent = function () { return 0; };
+
+    scrollableArea.scrollThreshold(980);
+    scrollableArea.contentSize(1000);
+    scrollableArea.scrollOffset(20);
+    scrollableArea.calculatedScrollValue(-100);
+
+    equal(scrollableArea.scrollOffset(), 20, " scrollableArea scrollOffset should be 20");
+    equal(scrollableArea.calculatedScrollValue(), -100, " scrollableArea calculatedScrollValue should be set to -100");
+    equal(scrollableArea.scrollValue(), -80, " scrollableArea calculatedScrollValue should be set to -80");
 });
